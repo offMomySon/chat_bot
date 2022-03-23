d705a317610cf669e14119784dbbce7f037293e5
오지훈 <huni1006@lezhin.com>
2022-03-24 08:53:45 +0900

    create cal cmd meta checker

---
+++ src/main/java/com/jihun/chat_bot/metaChecker/CalMetaChecker.java
@@ -0,0 +1,26 @@
+package com.jihun.chat_bot.metaChecker;
+
+import java.util.Set;
+import org.apache.commons.lang3.math.NumberUtils;
+
+public class CalMetaChecker {
+    private static final Set<String> operators = Set.of("+", "-", "*", "/");
+    private static final int metaLength = 3;
+    
+    public boolean isMatch(String[] meta) {
+        if (meta.length != metaLength) {
+            return false;
+        }
+
+        String leftOperand = meta[0];
+        String operator = meta[1];
+        String rightOperand = meta[2];
+
+        if (NumberUtils.isParsable(leftOperand) && NumberUtils.isParsable(rightOperand) &&
+            operators.contains(operator)) {
+            return true;
+        }
+
+        return false;
+    }
+}
