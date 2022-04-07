47f58833248870addb66f2bd1ba60a1cc50893c7
ks.choi <dv.mommoo@gmail.com>
2022-04-08 05:52:08 +0900

    feat: 문자열 메타 래퍼 객체 구현

---
+++ src/main/java/com/jihun/chat_bot/cmdChekcer/metaChecker/system/v4/meta/Meta.java
@@ -0,0 +1,64 @@
+package com.jihun.chat_bot.cmdChekcer.metaChecker.system.v4.meta;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Meta {
+    private final Set<String> values;
+
+    private Meta(Set<String> values) {
+        this.values = toLowercaseValues(values);
+    }
+
+    private static Set<String> toLowercaseValues(Set<String> values) {
+        if (Objects.isNull(values)) {
+            return new LinkedHashSet<>();
+        }
+
+        Set<String> newValues = values.stream()
+                .filter(Objects::nonNull)
+                .map(String::toLowerCase)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        return Collections.unmodifiableSet(newValues);
+    }
+
+    public static Meta from(String... values) {
+        return new Meta(
+                Stream.of(values)
+                .collect(Collectors.toCollection(LinkedHashSet::new))
+        );
+    }
+
+    public boolean contain(Meta givenMeta) {
+        return givenMeta.values
+                .stream()
+                .anyMatch(values::contains);
+    }
+
+    public List<String> getValues() {
+        return values.stream()
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    public Meta combine(Meta meta) {
+        LinkedHashSet<String> newValues = new LinkedHashSet<>(this.values);
+        newValues.addAll(meta.values);
+
+        return new Meta(newValues);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Meta)) return false;
+        Meta meta = (Meta) o;
+        return values.equals(meta.values);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(values);
+    }
+}
---
+++ src/test/java/com/jihun/chat_bot/cmdChekcer/metaChecker/system/v4/meta/MetaTest.java
@@ -0,0 +1,92 @@
+package com.jihun.chat_bot.cmdChekcer.metaChecker.system.v4.meta;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MetaTest {
+    private static final String[] META_VALUES = {"m", "meta"};
+
+    @DisplayName("메타 데이터가 하나라도 일치하는지 테스트 합니다")
+    @ParameterizedTest
+    @ValueSource(strings = {"m", "meta"})
+    void testIsValueAnyOneContain(String givenValue) {
+        // given
+        Meta givenMeta = Meta.from(givenValue);
+        Meta meta = Meta.from(META_VALUES);
+
+        // when
+        boolean acutal = meta.contain(givenMeta);
+
+        // then
+        assertThat(acutal).isTrue();
+    }
+
+    @DisplayName("메타 데이터는 대소문자를 가라지 않습니다")
+    @ParameterizedTest
+    @ValueSource(strings = {"M", "Meta"})
+    void testIsValueContainWithIgnoreCase(String givenValue) {
+        // given
+        Meta givenMeta = Meta.from(givenValue);
+        Meta meta = Meta.from(META_VALUES);
+
+        // when
+        boolean acutal = meta.contain(givenMeta);
+
+        // then
+        assertThat(acutal).isTrue();
+    }
+
+    @DisplayName("넣은 순서대로 값이 조회되는지 테스트 합니다")
+    @ParameterizedTest
+    @CsvSource("a, b, c")
+    void testIsGetValidSequenceValues(String value1, String value2, String value3) {
+        // given
+        Meta meta = Meta.from(value1, value2, value3);
+        List<String> expect = List.of(value1, value2, value3);
+
+        // when
+        List<String> actual = meta.getValues();
+
+        // then
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    @DisplayName("메타끼리 합병이 되는지 테스트 합니다")
+    @ParameterizedTest
+    @CsvSource("a, b, c")
+    void testCombineMetas(String value1, String value2, String value3) {
+        // given
+        Meta expect = Meta.from(value1, value2, value3);
+        Meta givenMeta1 = Meta.from(value1, value2);
+        Meta givenMeta2 = Meta.from(value3);
+
+        // when
+        Meta actual = givenMeta1.combine(givenMeta2);
+
+        // then
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    @DisplayName("메타끼리 합병 순서에따라 순서대로 값이 조회되는지 테스트 합니다")
+    @ParameterizedTest
+    @CsvSource("a, b, c")
+    void testWhenCombineMetasThenIsGetValidSequenceValues(String value1, String value2, String value3) {
+        // given
+        Meta givenMeta1 = Meta.from(value1, value2);
+        Meta givenMeta2 = Meta.from(value3);
+        List<String> expect = List.of(value1, value2, value3);
+
+        // when
+        Meta combineMeta = givenMeta1.combine(givenMeta2);
+        List<String> actual = combineMeta.getValues();
+
+        // then
+        assertThat(actual).isEqualTo(expect);
+    }
+}
