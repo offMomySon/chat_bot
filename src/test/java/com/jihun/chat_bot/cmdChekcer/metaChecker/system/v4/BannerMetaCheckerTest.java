cf44848234537f7326cd9f8dac5b8a6cfd491a0e
ks.choi <dv.mommoo@gmail.com>
2022-04-08 04:24:37 +0900

    refactor: 공통 메타 테스트 적용

--- src/test/java/com/jihun/chat_bot/cmdChekcer/metaChecker/system/v4/BannerMetaCheckerTest.java
+++ src/test/java/com/jihun/chat_bot/cmdChekcer/metaChecker/system/v4/BannerMetaCheckerTest.java
@@ -1,66 +1,31 @@
 package com.jihun.chat_bot.cmdChekcer.metaChecker.system.v4;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import java.util.Collection;
+import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-public class BannerMetaCheckerTest {
-    @DisplayName("올바른 배너 메타의 유효성검사를 진행합니다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"b", "banner"})
-    void testIsValidBannerMetaWhenGivenValidBannerMeta(String meta) {
-        // given
-        BannerMetaChecker bannerMetaChecker = new BannerMetaChecker();
-
-        // when
-        MetaResult actual = bannerMetaChecker.valid(meta);
-
-        // then
-        assertThat(actual).isSameAs(MetaResult.ALL_MATCHED);
+public class BannerMetaCheckerTest extends CommonMetaCheckerTest {
+    @Override
+    protected MetaChecker getMetaChecker() {
+        return new BannerMetaChecker();
     }
 
-    @DisplayName("부분적으로 일치하거나 전부 일치하면 전부 일치한 결과로 검사합니다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"banner"})
-    void testIsValidBannerMetaWhenGivenBannerMetaIsBothMatchedPartialAndAll(String meta) {
-        // given
-        BannerMetaChecker bannerMetaChecker = new BannerMetaChecker();
-
-        // when
-        MetaResult actual = bannerMetaChecker.valid(meta);
-
-        // then
-        assertThat(actual).isSameAs(MetaResult.ALL_MATCHED);
+    @Override
+    protected Collection<String> getAllMatchMetas() {
+        return List.of("b", "banner");
     }
 
-
-    @DisplayName("올바르지 않은 배너 메타의 유효성검사를 진행합니다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"dd", "d", "danners"})
-    void testIsInvalidBannerMetaWhenGivenInvalidBannerMeta(String meta) {
-        // given
-        BannerMetaChecker bannerMetaChecker = new BannerMetaChecker();
-
-        // when
-        MetaResult actual = bannerMetaChecker.valid(meta);
-
-        // then
-        assertThat(actual).isSameAs(MetaResult.NONE_MATCHED);
+    @Override
+    protected Collection<String> getPartialAndAllMatchMetas() {
+        return List.of("banner");
     }
 
-    @DisplayName("부분적으로 올바른 배너 메타의 유효성검사를 진행합니다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"bb", "brnner"})
-    void testIsPartialValidBannerMetaWhenGivenInvalidBannerMeta(String meta) {
-        // given
-        BannerMetaChecker bannerMetaChecker = new BannerMetaChecker();
+    @Override
+    protected Collection<String> getPartialMatchMetas() {
+        return List.of("bb", "brnner");
+    }
 
-        // when
-        MetaResult actual = bannerMetaChecker.valid(meta);
-
-        // then
-        assertThat(actual).isSameAs(MetaResult.PARTIAL_MATCHED);
+    @Override
+    protected Collection<String> getNonMatchMetas() {
+        return List.of("dd", "d", "danners");
     }
 }
--- src/test/java/com/jihun/chat_bot/cmdChekcer/metaChecker/system/v4/MetaCheckerTest.java
+++ src/test/java/com/jihun/chat_bot/cmdChekcer/metaChecker/system/v4/MetaCheckerTest.java
@@ -1,76 +1,38 @@
 package com.jihun.chat_bot.cmdChekcer.metaChecker.system.v4;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-public class MetaCheckerTest {
+public class MetaCheckerTest extends CommonMetaCheckerTest{
     //Note. 데이터의 순서 변경시 테스트의 의도가 깨질 수 있습니다.
     private static final List<String> TEST_METAS = List.of("t", "test", "m", "meta");
 
-    private MetaChecker metaChecker;
-
-    @BeforeEach
-    void setUpMetaChecker() {
-        this.metaChecker = new TestMetaChecker(TEST_METAS);
+    @Override
+    protected MetaChecker getMetaChecker() {
+        return new TestMetaChecker(TEST_METAS);
     }
 
-    @DisplayName("올바른 배너 메타의 유효성검사를 진행합니다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"t", "test", "m", "meta"})
-    void testIsValidBannerMetaWhenGivenValidBannerMeta(String meta) {
-        // given
-        // when
-        MetaResult actual = metaChecker.valid(meta);
-
-        // then
-        assertThat(actual).isSameAs(MetaResult.ALL_MATCHED);
+    @Override
+    protected Collection<String> getAllMatchMetas() {
+        return List.of("t", "test", "m", "meta");
     }
 
-    @DisplayName("부분적으로 일치하거나 전부 일치하면 전부 일치한 결과로 검사합니다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"test", "meta"})
-    void testIsValidBannerMetaWhenGivenBannerMetaIsBothMatchedPartialAndAll(String meta) {
-        // given
-        // when
-        MetaResult actual = metaChecker.valid(meta);
-
-        // then
-        assertThat(actual).isSameAs(MetaResult.ALL_MATCHED);
+    @Override
+    protected Collection<String> getPartialAndAllMatchMetas() {
+        return List.of("test", "meta");
     }
 
-
-    @DisplayName("올바르지 않은 배너 메타의 유효성검사를 진행합니다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"b", "ba", "banner"})
-    void testIsInvalidBannerMetaWhenGivenInvalidBannerMeta(String meta) {
-        // given
-        // when
-        MetaResult actual = metaChecker.valid(meta);
-
-        // then
-        assertThat(actual).isSameAs(MetaResult.NONE_MATCHED);
+    @Override
+    protected Collection<String> getPartialMatchMetas() {
+        return List.of("tt", "mm");
     }
 
-    @DisplayName("부분적으로 올바른 배너 메타의 유효성검사를 진행합니다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"tt", "mm"})
-    void testIsPartialValidBannerMetaWhenGivenInvalidBannerMeta(String meta) {
-        // given
-        // when
-        MetaResult actual = metaChecker.valid(meta);
-
-        // then
-        assertThat(actual).isSameAs(MetaResult.PARTIAL_MATCHED);
+    @Override
+    protected Collection<String> getNonMatchMetas() {
+        return List.of("b", "ba", "banner");
     }
 
     private static class TestMetaChecker extends MetaChecker {
